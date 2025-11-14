3c79cb38abda59b2d8b25ce0bf4c9ce2ee25a012
Joy Jean <joy@redhat.com>
2025-11-13 18:19:20 -0800

    Fix page refresh issue on GitHub Pages with 404.html redirect solution

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = process.env.BUILD_ENV === 'production';
@@ -44,6 +45,11 @@ module.exports = (env, argv) => {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       base: ASSET_PATH,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.', noErrorOnMissing: true }
+      ],
     }),
   ],
 };
